c54d5dfa0e79120a8c5eb8de488e0f4f9c40d287
h1 <154380480+Mrengin1@users.noreply.github.com>
2024-05-02 20:42:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const birthDate = new Date('2010-05-18'); 
-const creationDate = new Date('2024-04-1'); 
+const creationDate = new Date('2024-04-2'); 
 
 function calculateAge() {
   const currentDate = new Date();
