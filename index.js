a975e59b4f18da7ab06dbec90b5ecb332d17a610
h1 <154380480+Mrengin1@users.noreply.github.com>
2024-05-18 00:05:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function calculateTimeElapsed() {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
-  const months = Math.floor(days / 30.44); 
+  const months = Math.floor(days / 30); 
   const years = Math.floor(months / 12);
 
-  return { years, months: months % 12, days: days % 30.44, hours: hours % 24, minutes: minutes % 60, seconds: seconds % 60 };
+  return { years, months: months % 12, days: days % 30, hours: hours % 24, minutes: minutes % 60, seconds: seconds % 60 };
 }
 
 function updateAgeAndTimer() {
